2a6a39cb6ab1276c1d69ff9313fefc2afb99e6c9
dokolab <dokolab@gmail.com>
2018-01-17 20:23:18 +0900

    選択画面の機能実装

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+
+public class Player : MonoBehaviour {
+
+	// パネル1のシーン遷移（パネルに触れていて、aボタンを押したら）
+    void update()
+    {
+       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "panel1")
+        {
+            if (OVRInput.GetDown(OVRInput.RawButton.A))
+            {
+                Debug.Log("シーン1遷移");
+            }
+        }
+
+        if (collision.gameObject.name == "panel2")
+        {
+            if (OVRInput.GetDown(OVRInput.RawButton.A))
+            {
+                Debug.Log("シーン2遷移");
+            }
+        }
+    }
+
+}
